2e330e558f450dce654649e7e49a55092dab3687
luciferr <luciferrkr@gmail.com>
2023-06-18 02:08:48 +0900

    mod for error IGNORE_MOD_TAP_INTERRUPT

--- keymaps/arc/config.h
+++ keymaps/arc/config.h
@@ -7,7 +7,7 @@
 #define TAPPING_TERM 200
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
+// #define IGNORE_MOD_TAP_INTERRUPT
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
